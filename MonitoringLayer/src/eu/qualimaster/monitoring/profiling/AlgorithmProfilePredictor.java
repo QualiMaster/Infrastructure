4619d4b28b0ccfe72761c0294b50eb0f4714c3f9
Christopher Voges <voges@sse.uni-hildesheim.de>
2016-08-05 14:27:41 +0200

    Completed the implementation remarks for AlgorithmProfilePredictor.

--- MonitoringLayer/src/eu/qualimaster/monitoring/profiling/AlgorithmProfilePredictor.java
+++ MonitoringLayer/src/eu/qualimaster/monitoring/profiling/AlgorithmProfilePredictor.java
@@ -54,17 +54,22 @@ public class AlgorithmProfilePredictor {
     public static void notifyPipelineLifecycleChange(PipelineLifecycleEvent event) {
         //System.err.println(event);
         /*
-         * Use-Case
-         * The pipeline is STARTED, STOPPED or SWITCHED (i.e. one starts, another stops)
+         * Use-Case The pipeline is STARTED, STOPPED or SWITCHED (i.e. one
+         * starts, another stops)
          * 
-         * Implementation ideas:
-         * 1.   Get all data needed to identify the potentially Kalman-Instance(s) (starting and/or stopping)
-         * 2.   If STOP: Store the stopping Kalman-Instance
-         * 3.   If START: 
-         * 3a.  Load the (re)starting Kalman-Instance from ram/disk or 
-         * 3b.  Create a new Kalman-Instance 
-         *      (first: from scratch
-         *       later: as analogy, based on similar instances)
+         * 0. Continue only if the Pipeline did change, else: break/abort 
+         * Implementation ideas: 
+         * 1. Get all data needed to identify the potentially Kalman-Instance(s) 
+         * (starting and/or stopping) 
+         * 2. If STOP: Store the stopping Kalman-Instance 
+         * 3. If START: 
+         * 3a. Load the (re)starting Kalman-Instance from ram/disk or 
+         * 3b. Create a new Kalman-Instance 
+         *  (first: from scratch 
+         *  later: as analogy, based on similar instances)
+         * 
+         * Assumption: Only the Pipeline changes. The Algorithm and IObsevables
+         * (incl. EXECUTOTRS and TASKS) stay the same.
          */
     }
 
@@ -79,7 +84,20 @@ public class AlgorithmProfilePredictor {
         event.getPipelineElement();
         /*
          * Use-Case
-         * A Algorithm changes, i.e. 
+         * An Algorithm changes.
+         * 
+         * Implementation ideas: 
+         * 0. Continue only if the Algorithm did change, else: break/abort 
+         * 1. Get all data needed to identify the Kalman-Instances 
+         * (old and new Algorithm) 
+         * 2. Store the old Algorithms Kalman-Instance to ram/disk.
+         * 3. For the new Algorithm:  
+         * 3a. Load the (re)starting Kalman-Instance from ram/disk or 
+         * 3b. Create a new Kalman-Instance 
+         *  (first: from scratch, later: as analogy, based on similar instances)
+         * 
+         * Assumption: Only the Algorithm changes. The Pipeline and IObsevables
+         * (incl. EXECUTOTRS and TASKS) stay the same.
          */
     }
 
@@ -95,6 +113,25 @@ public class AlgorithmProfilePredictor {
         event.getPipelineElement();
         event.getParameter();
         event.getValue();
+        
+        /*
+         * Use-Case: A Kalman-Instance defining Parameter changes.
+         * Such Paramters are EXECUTORS, TASKS or the monitored/predicted IObservable (e.g. latency).
+         *  
+         * Implementation ideas:
+         * 0. Continue only if one or more Parameters changed, else: break/abort 
+         * 1. Get all data needed to identify the Kalman-Instances 
+         * (old and new Algorithm) 
+         * 2. Store the old Kalman-Instance to ram/disk.
+         * 3. For the new Algorithm:  
+         * 3a. Load the (re)starting Kalman-Instance from ram/disk or 
+         * 3b. Create a new Kalman-Instance 
+         *  (first: from scratch, later: as analogy, based on similar instances)
+         * 
+         * Assumption: Only EXECUTORS and/or TASKS changes. The Pipeline, Algorithm
+         * and monitored/predicted IObservabklstay the same.
+         * 
+         */
     }
 
     /**
@@ -106,6 +143,21 @@ public class AlgorithmProfilePredictor {
      */
     public static void notifyAlgorithmProfilingEvent(AlgorithmProfilingEvent event) {
         MonitoringConfiguration.getProfilingLogLocation(); 
+        /*
+         * Use-Case: Creating Kalman-Instances for later use.
+         * 
+         * Implementation ideas: 
+         * 1. Get all data needed to identify the Kalman-Instance 
+         * 2a. If START: Create a new Kalman-Instance
+         * 2b. If NEXT:  Load the already existing corresponding Kalman-Instance 
+         * 2c. If END: Store the Kalman-Instance 
+         * 3. If (START or NEXT) while (event.hasValues) updateKalman
+         * 
+         * Assumption: This method is (only) used for the batch-wise creation or 
+         * updating of Kalman-Instances in an non-productive environment (during profiling), i.e. 
+         * the total execution time can be longer than 500ms.
+         * 
+         */
     }
     
     /**
@@ -130,6 +182,20 @@ public class AlgorithmProfilePredictor {
                 dummy();
             }
         }
+        /*
+         * Use-Case:
+         * A new value for a specific IObservable (e.g. latency) was observed.
+         * 
+         * 1. yValue=value; xValue=(timestamp/now)
+         * 2. Get all data needed to identify the Kalman-Instance 
+         * 3. If Kalman-Instance is NOT running:  
+         * 3a. Load the (re)starting Kalman-Instance from ram/disk or 
+         * 3b. Create a new Kalman-Instance 
+         *  (first: from scratch, later: as analogy, based on similar instances)
+         * 4. Update the Kalman-Instance with (xValue, yValue)
+         * 
+         * Note: One Kalman-Instance handles one specific (measured) IObservable.
+         */
     }
 
     /**
@@ -148,6 +214,31 @@ public class AlgorithmProfilePredictor {
     public static double predict(String pipeline, String element, String algorithm, IObservable observable, 
         Map<Object, Serializable> targetValues) {
         return 0;
+        /*
+         * Use-Case:
+         * Get the prediction for a specific IObservable 
+         * one time-step (second) into the future.
+         * 
+         * Implementation idea:
+         * 1. If targetValues==null:
+         * 1a. Get all data needed to identify the Kalman-Instance then (If None: Abort; result=Double.MIN_VALUE)
+         * 1b. If Kalman-Instance is NOT running: Load it from ram/disk
+         * 1c: If Kalman-Instances last Update older than 1s: 
+         *      Call Gap-Closing method (could be handled internally by Kalman)
+         * 1d. Get predicted value ann return it
+         * OR
+         * 2: If targetValues!=null:
+         * 2a. Get all data needed to identify the Kalman-Instance then 
+         *      If None: Search for Kalman-Instances (Analog-Kalmans) corresponding to
+         *      the elements of targetValue
+         * 2b: IF Analog-Kalman==null: abort, result=Double.MIN_VALUE
+         * 2c: ELSE
+         *      Do 1a-1d and for all Instances in Analog-Kalman and interpolate the result.
+         * 2d. return the interpolated result
+         * 
+         * 
+         * Note: The second use case (2) is likely to take longer/too long.
+         */
     }
 
     /**
