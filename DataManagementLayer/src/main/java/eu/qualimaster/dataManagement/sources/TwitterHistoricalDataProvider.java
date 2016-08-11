805f7baccdd9f624537112852d5a1aa9e119449a
xander7 <ceroni@l3s.de>
2016-08-11 11:51:25 +0200

    Read and store data with HBase

--- DataManagementLayer/src/main/java/eu/qualimaster/dataManagement/sources/TwitterHistoricalDataProvider.java
+++ DataManagementLayer/src/main/java/eu/qualimaster/dataManagement/sources/TwitterHistoricalDataProvider.java
@@ -1,12 +1,19 @@
 package eu.qualimaster.dataManagement.sources;
 
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Serializable;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.util.Time;
 
 import eu.qualimaster.dataManagement.DataManager;
 import eu.qualimaster.dataManagement.storage.hbase.HBaseStorageSupport;
@@ -20,6 +27,7 @@ import eu.qualimaster.dataManagement.strategies.FirstInFirstOutStorageStrategyDe
  */
 public class TwitterHistoricalDataProvider implements IHistoricalDataProvider,Serializable
 {
+	private static final String DATE_FORMAT = "MM/DD/YYYY,hh:mm:ss";
 	private static final long serialVersionUID = 2128947946366967252L;
 
 	/**
@@ -32,11 +40,14 @@ public class TwitterHistoricalDataProvider implements IHistoricalDataProvider,Se
      */
     public void obtainHistoricalData(long timeHorizon, String term, File target) throws IOException
     {
+    	// get data using HBase
+    	List<String> data = getDataFromHBaseTable(timeHorizon, term);
     	
-    	
+    	// store data in the output file
+    	storeData(data, target);
     }
     
-    private void getDataFromHBaseTable(Long timeHorizon, String term)
+    private List<String> getDataFromHBaseTable(Long timeHorizon, String term)
     {
     	// Get the HBase table containing data for the input term
     	// TODO which strategy should be used?
@@ -45,25 +56,56 @@ public class TwitterHistoricalDataProvider implements IHistoricalDataProvider,Se
     	
     	// Get all the keys (dates) of the table and keep only those within the time horizon
     	List<Object> keys = table.getKeys();
-    	List<String> validKeys = filterKeys(keys, timeHorizon);
+    	List<String> validKeys = filterKeys(keys, System.currentTimeMillis() - timeHorizon);
     	
+    	// Get values for the remaining keys and put both in the same string
+    	List<String> data = new ArrayList<>();
+    	for(String key : validKeys) data.add(makeDataLine(key, table));
+    	table.disconnect();
+    	
+    	return data;
     }
     
-    private List<String> filterKeys(List<Object> keys, Long timeHorizon)
+    private String makeDataLine(String key, HBaseStorageSupport table)
+    {
+    	String value = (String) table.get(key);
+    	return key + "," + value;
+    }
+    
+    private List<String> filterKeys(List<Object> keys, Long minTime)
     {
     	List<String> filteredKeys = new ArrayList<String>();
     	for(Object o : keys)
     	{
     		byte[] key = (byte[]) o;
     		String keyString = Bytes.toString(key);
-    		if(isDateInHorizon(keyString, timeHorizon)) filteredKeys.add(keyString); 
+    		if(isDateInHorizon(keyString, minTime)) filteredKeys.add(keyString); 
     	}
     	return filteredKeys;
     }
     
-    private boolean isDateInHorizon(String date, Long timeHorizon)
+    private boolean isDateInHorizon(String dateStr, Long minTime)
     {
     	// check if the date represented by the input string is within the input time horizon
-    	return true;
+    	DateFormat format = new SimpleDateFormat(DATE_FORMAT);
+    	try {
+			Date date = format.parse(dateStr);
+			if(date.getTime() > minTime) return true;
+			else return false;
+		}
+    	catch (ParseException e) {
+			e.printStackTrace();
+			return false;
+		}
+    }
+    
+    private void storeData(List<String> data, File outputFile) throws IOException{
+    	BufferedWriter writer = null;
+		writer = new BufferedWriter(new FileWriter(outputFile));
+		for(String s : data){
+			writer.write(s);
+			writer.newLine();
+		}
+		writer.close();
     }
 }
