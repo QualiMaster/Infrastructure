58616a8560573b388ddb36ba581d061cbd461da0
xander7 <ceroni@l3s.de>
2016-08-22 08:41:45 +0200

    Removed the fixed names of the sources.

--- MonitoringLayer/src/eu/qualimaster/monitoring/volumePrediction/VolumePredictionManager.java
+++ MonitoringLayer/src/eu/qualimaster/monitoring/volumePrediction/VolumePredictionManager.java
@@ -10,49 +10,45 @@ import eu.qualimaster.infrastructure.PipelineLifecycleEvent;
 import eu.qualimaster.monitoring.events.SourceVolumeMonitoringEvent;
 
 /**
- * Entrance point for the volume prediction. It handles the events and contains 2 sets of predictors (for Twitter and Spring) separately.
+ * Entrance point for the volume prediction, it handles the events and provides the methods to use the functionalities of the volume prediction.
+ * It contains a source-predictor map to efficiently resolve which predictor to use based on the source.
  * 
  * @author  Andrea Ceroni
  */
 public class VolumePredictionManager {
 
 	/**
-	 * Volume predictor for Spring (terms and corresponding predictors are stored as hashmap) 
+	 * Set of available volume predictors, one for each different source.
 	 */
-	private static VolumePredictor springVolumePredictor = new VolumePredictor(SourceName.SPRING) ;
-	
-	/**
-	 * Volume predictor for Twitter (terms and corresponding predictors are stored ad hashmap)
-	 */
-	private static VolumePredictor twitterVolumePredictor = new VolumePredictor(SourceName.TWITTER);
+	private static HashMap<String,VolumePredictor> volumePredictors = new HashMap<>();
 	
 	/**
 	 * Initializes the volume predictor for a given source (either Spring or Twitter), assuming that the data provider has been already
 	 * set via the proper event. This must be called before feeding the predictor with volume data, with enough advance to let the 
 	 * predictors (one for each input term) be trained.
-	 * @param s the source (either Spring or Twitter) the predictor will refer to.
+	 * @param s the source the predictor will refer to.
 	 * @param monitoredTerms the initial set of terms to be monitored (with their volume thresholds) and for which a predictor must be 
 	 * trained. It can be null or empty, in this case the predictor will not be able to make any prediction and will need to be updated at some point. 
 	 * @param blindTerms the initial set of terms whose historical volume can looked up. It can be null or empty. 
 	 * @param path the path of a temporary file used within the predictor to store and read data.
 	 */
-	public static void initialize(SourceName s, HashMap<String,Long> monitoredTerms, HashSet<String> blindTerms, String path){
-		if(s == SourceName.SPRING) springVolumePredictor.initialize(monitoredTerms, blindTerms, path);
-		else if(s == SourceName.TWITTER) twitterVolumePredictor.initialize(monitoredTerms, blindTerms, path);
-		else System.out.println("ERROR: the input source is not handled.");
+	public static void initialize(String source, HashMap<String,Long> monitoredTerms, HashSet<String> blindTerms, String path){
+		VolumePredictor predictor = volumePredictors.get(source);
+		if(predictor != null) predictor.initialize(monitoredTerms, blindTerms, path);
+		else System.out.println("ERROR: no volume predictor available for the input source" + source);
 	}
 	
 	/**
 	 * Performs a blind prediction (i.e. only based on historical data, if available) for a given term in a given source
 	 * @param source the source containing the term
 	 * @param term the term whose volume has to be predicted
-	 * @return the predicted volume of the term; -1 if a model with historial data for the input term is not available. 
+	 * @return the predicted volume of the term; -1 if a model with historcal data for the input term is not available. 
 	 */
-	public static double predictBlindly(SourceName source, String term){
-		if(source == SourceName.SPRING) return springVolumePredictor.predictBlindly(term);
-		else if(source == SourceName.TWITTER) return twitterVolumePredictor.predictBlindly(term);
+	public static double predictBlindly(String source, String term){
+		VolumePredictor predictor = volumePredictors.get(source);
+		if(predictor != null) return predictor.predictBlindly(term);
 		else{
-			System.out.println("ERROR: the input source is not handled.");
+			System.out.println("ERROR: no volume predictor available for the input source" + source);
 			return -1;
 		}
 	}
@@ -61,8 +57,19 @@ public class VolumePredictionManager {
 	 * Updates the models for all the terms in all the sources.
 	 */
 	public static void updatePredictors(){
-		springVolumePredictor.updatePrediction();
-		twitterVolumePredictor.updatePrediction();
+		for(String source : volumePredictors.keySet()){
+			updatePredictorsForSource(source);
+		}
+	}
+	
+	/**
+	 * Updates the predictors for all the terms in a single source 
+	 * @param source
+	 */
+	public static void updatePredictorsForSource(String source){
+		VolumePredictor predictor = volumePredictors.get(source);
+		if(predictor != null) predictor.updatePrediction();
+		else System.out.println("ERROR: no volume predictor available for the input source" + source);
 	}
 	
 	/**
@@ -71,10 +78,10 @@ public class VolumePredictionManager {
 	 * @param term the new monitored term to be added
 	 * @param threshold the volume threshold for the new term
 	 */
-	public static void addMonitoredTerm(SourceName source, String term, long threshold){
-		if(source == SourceName.SPRING) springVolumePredictor.addMonitoredTerm(term, threshold);
-		else if(source == SourceName.TWITTER) twitterVolumePredictor.addMonitoredTerm(term, threshold);
-		else System.out.println("ERROR: the input source is not handled.");
+	public static void addMonitoredTerm(String source, String term, long threshold){
+		VolumePredictor predictor = volumePredictors.get(source);
+		if(predictor != null) predictor.addMonitoredTerm(term, threshold);
+		else System.out.println("ERROR: no volume predictor available for the input source" + source);
 	}
 	
 	/**
@@ -82,31 +89,31 @@ public class VolumePredictionManager {
 	 * @param source the source that the term belongs to
 	 * @param term the monitored term to be removed
 	 */
-	public static void removeMonitoredTerm(SourceName source, String term){
-		if(source == SourceName.SPRING) springVolumePredictor.removeMonitoredTerm(term);
-		else if(source == SourceName.TWITTER) twitterVolumePredictor.removeMonitoredTerm(term);
-		else System.out.println("ERROR: the input source is not handled.");
-	}
+	public static void removeMonitoredTerm(String source, String term){
+		VolumePredictor predictor = volumePredictors.get(source);
+		if(predictor != null) predictor.removeMonitoredTerm(term);
+		else System.out.println("ERROR: no volume predictor available for the input source" + source);
+		}
 	
 	/** Creates a blind model for a new term and adds it to the predictor.
 	 * @param source the source that the term belongs to
 	 * @param term the blind term to be added
 	 */
-	public static void addBlindTerm(SourceName source, String term){
-		if(source == SourceName.SPRING) springVolumePredictor.addBlindTerm(term);
-		else if(source == SourceName.TWITTER) twitterVolumePredictor.addBlindTerm(term);
-		else System.out.println("ERROR: the input source is not handled.");
-	}
+	public static void addBlindTerm(String source, String term){
+		VolumePredictor predictor = volumePredictors.get(source);
+		if(predictor != null) predictor.addBlindTerm(term);
+		else System.out.println("ERROR: no volume predictor available for the input source" + source);
+		}
 	
 	/**
 	 * Removes a blind term (and the corresponding bling model) from the predictor
 	 * @param source the source that the term belongs to
 	 * @param term the blind term to be removed
 	 */
-	public static void removeBlindTerm(SourceName source, String term){
-		if(source == SourceName.SPRING) springVolumePredictor.removeBlindTerm(term);
-		else if(source == SourceName.TWITTER) twitterVolumePredictor.removeBlindTerm(term);
-		else System.out.println("ERROR: the input source is not handled.");
+	public static void removeBlindTerm(String source, String term){
+		VolumePredictor predictor = volumePredictors.get(source);
+		if(predictor != null) predictor.removeBlindTerm(term);
+		else System.out.println("ERROR: no volume predictor available for the input source" + source);
 	}
 	
 	/**
@@ -115,10 +122,10 @@ public class VolumePredictionManager {
 	 * @param term the monitored term whose threshold has to be changed
 	 * @param threshold the new volume threshold for the monitored term
 	 */
-	public static void updateTermThreshold(SourceName source, String term, long threshold){
-		if(source == SourceName.SPRING) springVolumePredictor.updateTermThreshold(term, threshold);
-		else if(source == SourceName.TWITTER) twitterVolumePredictor.updateTermThreshold(term, threshold);
-		else System.out.println("ERROR: the input source is not handled.");
+	public static void updateTermThreshold(String source, String term, long threshold){
+		VolumePredictor predictor = volumePredictors.get(source);
+		if(predictor != null) predictor.updateTermThreshold(term, threshold);
+		else System.out.println("ERROR: no volume predictor available for the input source" + source);
 	}
 	
 	/**
@@ -139,16 +146,8 @@ public class VolumePredictionManager {
 	    protected void handle(HistoricalDataProviderRegistrationEvent event) {
 	        // called when a data source comes up in a pipeline. Carries the historical data provider.
 	        // If the source changes, an event with the same pipeline / element name will occur
-	    	
-	    	if(event.getSource().equalsIgnoreCase(SourceName.SPRING.toString())){
-	    		VolumePredictionManager.springVolumePredictor.setHistoryProvider(event.getProvider());
-	    	}
-	    	else if(event.getSource().equalsIgnoreCase(SourceName.TWITTER.toString())){
-	    		VolumePredictionManager.twitterVolumePredictor.setHistoryProvider(event.getProvider());
-	    	}
-	    	else{
-	    		System.out.println("ERROR: the input source is unknown.");
-	    	}
+	    	VolumePredictor predictor = new VolumePredictor(event.getSource(), event.getProvider());
+	    	volumePredictors.put(event.getSource(), predictor);
 	    }
 	}
 
@@ -166,17 +165,10 @@ public class VolumePredictionManager {
 		
 		// TODO handle exceptions like: source map does not contain an input term; the model for a source is not available (null);
 		
-		// use the right predictor based on the source
-		VolumePredictor predictor = null;
-		if(event.getPipelineElement().equalsIgnoreCase(SourceName.SPRING.toString())) predictor = springVolumePredictor;
-		else if(event.getPipelineElement().equalsIgnoreCase(SourceName.TWITTER.toString())) predictor = twitterVolumePredictor;
-		else{
-    		System.out.println("ERROR: the input source is unknown.");
-    		return;
-    	}
-		
-		// handle the prediction for the incoming terms
-		predictor.handlePredictionStep(event.getObservations());
+		// use the right predictor (based on the source) to handle the prediction for the incoming terms
+		VolumePredictor predictor = volumePredictors.get(event.getPipelineElement());
+		if(predictor != null) predictor.handlePredictionStep(event.getObservations());
+		else System.out.println("ERROR: no volume predictor available for the input source" + event.getPipelineElement());
 	}
 	    
 	/**
@@ -198,9 +190,5 @@ public class VolumePredictionManager {
 	* Called upon shutdown of the infrastructure. Clean up global resources here.
 	*/
 	public static void stop() {
-	}
-	
-	public enum SourceName {
-	    SPRING, TWITTER 
 	}
 }
