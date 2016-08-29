aa93786872a55c6358232bd5b22ebca9b75b9d4c
Christopher Voges <voges@sse.uni-hildesheim.de>
2016-08-29 07:39:29 +0200

    Added gap handling, loading/storing methods, initial matrices and the relating parameter/distribution space to existing/persisted matrices (i.e. profiles)

--- MonitoringLayer/src/eu/qualimaster/monitoring/profiling/Kalman.java
+++ MonitoringLayer/src/eu/qualimaster/monitoring/profiling/Kalman.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2009-2016 University of Hildesheim, Software Systems Engineering
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package eu.qualimaster.monitoring.profiling;
 import java.util.ArrayList;
 
@@ -115,11 +130,6 @@ public class Kalman implements AlgorithmProfilePredictorAlgorithm {
      * zero-vector.
      */
     private RealVector controlVector = MatrixUtils.createRealVector(new double[] {0, 0, 0, 0});
-    
-    /**
-     * The point in time this Kalman-Instance was first created as UTC milliseconds.
-     */
-    private final long startTime = System.currentTimeMillis();    
     
     /**
      * The point in time this Kalman-Instance was last updated as seconds since midnight, January 1, 1970 UTC.
