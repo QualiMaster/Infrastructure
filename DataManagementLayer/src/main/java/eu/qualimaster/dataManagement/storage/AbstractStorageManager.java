c9b8dc7b88db298d714f73b219021551523cb236
tahoangl3s <hoang@l3s.de>
2016-09-23 22:19:03 +0200

    modify getTable

--- DataManagementLayer/src/main/java/eu/qualimaster/dataManagement/storage/AbstractStorageManager.java
+++ DataManagementLayer/src/main/java/eu/qualimaster/dataManagement/storage/AbstractStorageManager.java
@@ -104,13 +104,25 @@ public abstract class AbstractStorageManager<T extends AbstractStorageTable> ext
 			msg.append(": ");
 			msg.append(tables.get(k).getClass().toString());
 		}
-		throw new RuntimeException("The table mapping: " + msg.toString());
+		T result = tables.get(key);
+
+		if (null != result) {
+			throw new RuntimeException("The table mapping: " + msg.toString());
+		}
+
 		/*
-		 * T result = tables.get(key); if (null == result) { result =
-		 * createTable(validateTableName(key)); // we do not use create here /
-		 * parameters System.out.println("result " + result); if (null !=
+		 * if (null == result) {
+		 * 
+		 * result = createTable(validateTableName(key)); // we do not use create
+		 * here / parameters System.out.println("result " + result); if (null !=
 		 * result) { result.setStrategy(strategy); register(unit, new
 		 * LocalReference<T>(result)); } } return result;
 		 */
+
+		else {
+			result = createTable(validateTableName(key));
+			throw new RuntimeException(
+					"The table mapping: " + msg.toString() + ". The result type: " + result.getClass().toString());
+		}
 	}
 }
