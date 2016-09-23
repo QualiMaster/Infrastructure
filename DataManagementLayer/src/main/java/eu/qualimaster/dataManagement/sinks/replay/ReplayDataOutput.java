d7779be829aa941c7ebd2cfba8ae9dc5f4b86da9
tahoangl3s <hoang@l3s.de>
2016-09-23 17:16:52 +0200

    update error message in ReplayDataOuput

--- DataManagementLayer/src/main/java/eu/qualimaster/dataManagement/sinks/replay/ReplayDataOutput.java
+++ DataManagementLayer/src/main/java/eu/qualimaster/dataManagement/sinks/replay/ReplayDataOutput.java
@@ -13,9 +13,9 @@ import eu.qualimaster.dataManagement.storage.hbase.HBaseBatchStorageSupport;
 import eu.qualimaster.dataManagement.storage.support.IStorageSupport;
 import org.apache.hadoop.hbase.util.Bytes;
 
-/** 
- * The data output that buffers data from the replay recorder
- * and puts them into the underlying replay store
+/**
+ * The data output that buffers data from the replay recorder and puts them into
+ * the underlying replay store
  *
  * @author tuan
  * @version 1.0
@@ -23,356 +23,359 @@ import org.apache.hadoop.hbase.util.Bytes;
 
 public class ReplayDataOutput implements IDataOutput, Closeable {
 
-    /**
-     * Field are used to match to the column (or in Hbase,
-     * the column qualifiers)
-     */
-    private Field[] fields;
+	/**
+	 * Field are used to match to the column (or in Hbase, the column
+	 * qualifiers)
+	 */
+	private Field[] fields;
 
-    /** Cursor to the current field */
-    private int idx;
+	/** Cursor to the current field */
+	private int idx;
 
-    /** 2016-06-02: current version uses an HBase table */
-    private HBaseBatchStorageSupport storer;
+	/** 2016-06-02: current version uses an HBase table */
+	private HBaseBatchStorageSupport storer;
 
-    /** Hold values of the current row to be written */
-    private HBaseBatchStorageSupport.HBaseRow row;
+	/** Hold values of the current row to be written */
+	private HBaseBatchStorageSupport.HBaseRow row;
 
-    private StringBuilder keyBuilder;
+	private StringBuilder keyBuilder;
 
-    private long timestamp;
+	private long timestamp;
 
-    public ReplayDataOutput(Tuple schema, IStorageSupport storer) {
+	public ReplayDataOutput(Tuple schema, IStorageSupport storer) {
 
-        // Current version hooks with HBaseBatchStorageSupport
-        if (!(storer instanceof HBaseBatchStorageSupport)) {
-            throw new RuntimeException("Invalid replay store: "
-            + "Current version only works with HBase");
-        }
-        this.storer = (HBaseBatchStorageSupport)storer;
-        this.storer.connect();
-        this.fields = new Field[schema.getFields().size()];
-        this.fields = schema.getFields().toArray(fields);
-        row = new HBaseBatchStorageSupport.HBaseRow();
-        for (Field f : fields) {
-            if (!f.isKey() && !f.isTimesamp()) {
-                row.addColumn(Bytes.toBytes(f.getName()));
-            }
-        }
-        keyBuilder = new StringBuilder();
-    }
+		// Current version hooks with HBaseBatchStorageSupport
+		if (!(storer instanceof HBaseBatchStorageSupport)) {
+			throw new RuntimeException("Invalid replay store: "
+					+ "Current version only works with HBase. The provided is " + storer.getClass().toString());
+		}
 
-    private void writeIfNeeded() throws UnsupportedEncodingException {
-        idx++;
-        if (idx == fields.length) {
-            keyBuilder.append(DELIMITER);
+		this.storer = (HBaseBatchStorageSupport) storer;
+		this.storer.connect();
+		this.fields = new Field[schema.getFields().size()];
+		this.fields = schema.getFields().toArray(fields);
+		row = new HBaseBatchStorageSupport.HBaseRow();
+		for (Field f : fields) {
+			if (!f.isKey() && !f.isTimesamp()) {
+				row.addColumn(Bytes.toBytes(f.getName()));
+			}
+		}
+		keyBuilder = new StringBuilder();
+	}
 
-            /* In HBase, we append row key with the time step
-             * to facilitate range queries */
-            keyBuilder.append(String.valueOf(timestamp));
-            byte[] bytes = keyBuilder.toString().getBytes("UTF-8");
-            row.setKey(bytes);
-            storer.write(row);
-            keyBuilder.delete(0, keyBuilder.length());
-            row.resetData();
-            idx = 0;
-        }
-    }
+	private void writeIfNeeded() throws UnsupportedEncodingException {
+		idx++;
+		if (idx == fields.length) {
+			keyBuilder.append(DELIMITER);
 
-    private void appendToKey(String key) {
-        if (keyBuilder.length() > 0) {
-            keyBuilder.append(DELIMITER);
-        }
-        keyBuilder.append(key);
-    }
+			/*
+			 * In HBase, we append row key with the time step to facilitate
+			 * range queries
+			 */
+			keyBuilder.append(String.valueOf(timestamp));
+			byte[] bytes = keyBuilder.toString().getBytes("UTF-8");
+			row.setKey(bytes);
+			storer.write(row);
+			keyBuilder.delete(0, keyBuilder.length());
+			row.resetData();
+			idx = 0;
+		}
+	}
 
-    private void appendToKey(char key) {
-        if (keyBuilder.length() > 0) {
-            keyBuilder.append(DELIMITER);
-        }
-        keyBuilder.append(key);
-    }
+	private void appendToKey(String key) {
+		if (keyBuilder.length() > 0) {
+			keyBuilder.append(DELIMITER);
+		}
+		keyBuilder.append(key);
+	}
 
-    private void appendToKey(long[] key) {
-        if (keyBuilder.length() > 0) {
-            keyBuilder.append(DELIMITER);
-        }
-        for (long k : key) {
-            keyBuilder.append(String.valueOf(k));
-        }
-    }
+	private void appendToKey(char key) {
+		if (keyBuilder.length() > 0) {
+			keyBuilder.append(DELIMITER);
+		}
+		keyBuilder.append(key);
+	}
 
-    private void appendToKey(int[] key) {
-        if (keyBuilder.length() > 0) {
-            keyBuilder.append(DELIMITER);
-        }
-        for (int k : key) {
-            keyBuilder.append(String.valueOf(k));
-        }
-    }
+	private void appendToKey(long[] key) {
+		if (keyBuilder.length() > 0) {
+			keyBuilder.append(DELIMITER);
+		}
+		for (long k : key) {
+			keyBuilder.append(String.valueOf(k));
+		}
+	}
 
-    private void appendToKey(short[] key) {
-        if (keyBuilder.length() > 0) {
-            keyBuilder.append(DELIMITER);
-        }
-        for (short k : key) {
-            keyBuilder.append(String.valueOf(k));
-        }
-    }
+	private void appendToKey(int[] key) {
+		if (keyBuilder.length() > 0) {
+			keyBuilder.append(DELIMITER);
+		}
+		for (int k : key) {
+			keyBuilder.append(String.valueOf(k));
+		}
+	}
 
-    private void appendToKey(char[] key) {
-        if (keyBuilder.length() > 0) {
-            keyBuilder.append(DELIMITER);
-        }
-        for (char k : key) {
-            keyBuilder.append(k);
-        }
-    }
+	private void appendToKey(short[] key) {
+		if (keyBuilder.length() > 0) {
+			keyBuilder.append(DELIMITER);
+		}
+		for (short k : key) {
+			keyBuilder.append(String.valueOf(k));
+		}
+	}
 
-    private void appendToKey(String[] key) {
-        if (keyBuilder.length() > 0) {
-            keyBuilder.append(DELIMITER);
-        }
-        for (String k : key) {
-            keyBuilder.append(k);
-        }
-    }
+	private void appendToKey(char[] key) {
+		if (keyBuilder.length() > 0) {
+			keyBuilder.append(DELIMITER);
+		}
+		for (char k : key) {
+			keyBuilder.append(k);
+		}
+	}
 
-    private void appendToKey(byte[] key) {
-        if (keyBuilder.length() > 0) {
-            keyBuilder.append(DELIMITER);
-        }
-        try {
-            String s = new String(key, "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException(e);
-        }
-    }
+	private void appendToKey(String[] key) {
+		if (keyBuilder.length() > 0) {
+			keyBuilder.append(DELIMITER);
+		}
+		for (String k : key) {
+			keyBuilder.append(k);
+		}
+	}
+
+	private void appendToKey(byte[] key) {
+		if (keyBuilder.length() > 0) {
+			keyBuilder.append(DELIMITER);
+		}
+		try {
+			String s = new String(key, "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 	@Override
 	public void writeInt(int value) throws IOException {
-        byte[] bytes = Bytes.toBytes(value);
-        if (fields[idx].isKey()) {
-            appendToKey(String.valueOf(value));
-        } else if (fields[idx].isTimesamp()) {
-            timestamp = ReplayUtils.getTimestamp(fields[idx], value);
-        } else {
-            row.addValue(bytes);
-        }
-        writeIfNeeded();
+		byte[] bytes = Bytes.toBytes(value);
+		if (fields[idx].isKey()) {
+			appendToKey(String.valueOf(value));
+		} else if (fields[idx].isTimesamp()) {
+			timestamp = ReplayUtils.getTimestamp(fields[idx], value);
+		} else {
+			row.addValue(bytes);
+		}
+		writeIfNeeded();
 	}
 
 	@Override
 	public void writeLong(long value) throws IOException {
-        byte[] bytes = Bytes.toBytes(value);
-        if (fields[idx].isKey()) {
-            appendToKey(String.valueOf(value));
-        } else if (fields[idx].isTimesamp()) {
-            timestamp = ReplayUtils.getTimestamp(fields[idx], value);
-        } else {
-            row.addValue(bytes);
-        }
-        writeIfNeeded();
+		byte[] bytes = Bytes.toBytes(value);
+		if (fields[idx].isKey()) {
+			appendToKey(String.valueOf(value));
+		} else if (fields[idx].isTimesamp()) {
+			timestamp = ReplayUtils.getTimestamp(fields[idx], value);
+		} else {
+			row.addValue(bytes);
+		}
+		writeIfNeeded();
 	}
 
-    @Override
-    public void writeShort(short value) throws IOException {
-        byte[] bytes = Bytes.toBytes(value);
-        if (fields[idx].isKey()) {
-            appendToKey(String.valueOf(value));
-        } else if (fields[idx].isTimesamp()) {
-            timestamp = ReplayUtils.getTimestamp(fields[idx], value);
-        } else {
-            row.addValue(bytes);
-        }
-        writeIfNeeded();
-    }
+	@Override
+	public void writeShort(short value) throws IOException {
+		byte[] bytes = Bytes.toBytes(value);
+		if (fields[idx].isKey()) {
+			appendToKey(String.valueOf(value));
+		} else if (fields[idx].isTimesamp()) {
+			timestamp = ReplayUtils.getTimestamp(fields[idx], value);
+		} else {
+			row.addValue(bytes);
+		}
+		writeIfNeeded();
+	}
 
 	@Override
 	public void writeBoolean(boolean value) throws IOException {
-        byte[] bytes = Bytes.toBytes(value);
-        if (fields[idx].isKey()) {
-            appendToKey(value ? "0" : "1");
-        }
-        // boolean field cannot be timestamp field
-        else {
-            row.addValue(bytes);
-        }
-        writeIfNeeded();
+		byte[] bytes = Bytes.toBytes(value);
+		if (fields[idx].isKey()) {
+			appendToKey(value ? "0" : "1");
+		}
+		// boolean field cannot be timestamp field
+		else {
+			row.addValue(bytes);
+		}
+		writeIfNeeded();
 	}
 
 	@Override
 	public void writeDouble(double value) throws IOException {
-        byte[] bytes = Bytes.toBytes(value);
-        if (fields[idx].isKey()) {
-            appendToKey(String.valueOf(value));
-        }
-        // For the moment, double field cannot be timestamp field
-        else {
-            row.addValue(bytes);
-        }
-        writeIfNeeded();
+		byte[] bytes = Bytes.toBytes(value);
+		if (fields[idx].isKey()) {
+			appendToKey(String.valueOf(value));
+		}
+		// For the moment, double field cannot be timestamp field
+		else {
+			row.addValue(bytes);
+		}
+		writeIfNeeded();
 	}
 
 	@Override
 	public void writeFloat(float value) throws IOException {
-        byte[] bytes = Bytes.toBytes(value);
-        if (fields[idx].isKey()) {
-            appendToKey(String.valueOf(value));
-        }
-        // For the moment, float field cannot be timestamp field
-        else {
-            row.addValue(bytes);
-        }
-        writeIfNeeded();
+		byte[] bytes = Bytes.toBytes(value);
+		if (fields[idx].isKey()) {
+			appendToKey(String.valueOf(value));
+		}
+		// For the moment, float field cannot be timestamp field
+		else {
+			row.addValue(bytes);
+		}
+		writeIfNeeded();
 	}
 
 	@Override
 	public void writeChar(char value) throws IOException {
-        byte[] bytes = Bytes.toBytes(value);
-        if (fields[idx].isKey()) {
-            appendToKey(value);
-        }
-        // For the moment, char field cannot be timestamp field
-        else {
-            row.addValue(bytes);
-        }
-        writeIfNeeded();
+		byte[] bytes = Bytes.toBytes(value);
+		if (fields[idx].isKey()) {
+			appendToKey(value);
+		}
+		// For the moment, char field cannot be timestamp field
+		else {
+			row.addValue(bytes);
+		}
+		writeIfNeeded();
 	}
 
 	@Override
 	public void writeString(String value) throws IOException {
-        byte[] bytes = Bytes.toBytes(value);
-        if (fields[idx].isKey()) {
-            appendToKey(value);
-        }
-        else if (fields[idx].isTimesamp()) {
-            timestamp = ReplayUtils.getTimestamp(fields[idx], value);
-        } else {
-            row.addValue(bytes);
-        }
-        writeIfNeeded();
+		byte[] bytes = Bytes.toBytes(value);
+		if (fields[idx].isKey()) {
+			appendToKey(value);
+		} else if (fields[idx].isTimesamp()) {
+			timestamp = ReplayUtils.getTimestamp(fields[idx], value);
+		} else {
+			row.addValue(bytes);
+		}
+		writeIfNeeded();
 	}
 
 	@Override
 	public void writeByte(byte value) throws IOException {
-        byte[] bytes = new byte[]{value};
-        if (fields[idx].isKey()) {
-            appendToKey(String.valueOf(value));
-        }
-        // For the moment, byte field cannot be timestamp field
-        else {
-            row.addValue(bytes);
-        }
-        writeIfNeeded();
+		byte[] bytes = new byte[] { value };
+		if (fields[idx].isKey()) {
+			appendToKey(String.valueOf(value));
+		}
+		// For the moment, byte field cannot be timestamp field
+		else {
+			row.addValue(bytes);
+		}
+		writeIfNeeded();
 	}
 
 	@Override
 	public void writeLongArray(long[] array) throws IOException {
-        byte[] bytes = ReplayUtils.toBytes(array);
-        if (fields[idx].isKey()) {
-            appendToKey(array);
-        }
-        // For now, long array cannot be timestamp field
-        else {
-            row.addValue(bytes);
-        }
-        writeIfNeeded();
+		byte[] bytes = ReplayUtils.toBytes(array);
+		if (fields[idx].isKey()) {
+			appendToKey(array);
+		}
+		// For now, long array cannot be timestamp field
+		else {
+			row.addValue(bytes);
+		}
+		writeIfNeeded();
 	}
 
 	@Override
 	public void writeIntArray(int[] array) throws IOException {
-        byte[] bytes = ReplayUtils.toBytes(array);
-        if (fields[idx].isKey()) {
-            appendToKey(array);
-        }
-        // For now, int array cannot be timestamp field
-        else {
-            row.addValue(bytes);
-        }
-        writeIfNeeded();
+		byte[] bytes = ReplayUtils.toBytes(array);
+		if (fields[idx].isKey()) {
+			appendToKey(array);
+		}
+		// For now, int array cannot be timestamp field
+		else {
+			row.addValue(bytes);
+		}
+		writeIfNeeded();
 	}
 
 	@Override
 	public void writeBooleanArray(boolean[] array) throws IOException {
-        byte[] bytes = ReplayUtils.toBytes(array);
+		byte[] bytes = ReplayUtils.toBytes(array);
 
-        // For now, boolean array can neither be key nor timestamp
-        row.addValue(bytes);
-        writeIfNeeded();
+		// For now, boolean array can neither be key nor timestamp
+		row.addValue(bytes);
+		writeIfNeeded();
 	}
 
 	@Override
 	public void writeDoubleArray(double[] array) throws IOException {
-        byte[] bytes = ReplayUtils.toBytes(array);
+		byte[] bytes = ReplayUtils.toBytes(array);
 
-        // For now, double array can neither be key nor timestamp
-        row.addValue(bytes);
-        writeIfNeeded();
+		// For now, double array can neither be key nor timestamp
+		row.addValue(bytes);
+		writeIfNeeded();
 	}
 
 	@Override
 	public void writeFloatArray(float[] array) throws IOException {
-        byte[] bytes = ReplayUtils.toBytes(array);
+		byte[] bytes = ReplayUtils.toBytes(array);
 
-        // For now, float array can neither be key nor timestamp
-        row.addValue(bytes);
-        writeIfNeeded();
+		// For now, float array can neither be key nor timestamp
+		row.addValue(bytes);
+		writeIfNeeded();
 	}
 
 	@Override
 	public void writeShortArray(short[] array) throws IOException {
-        byte[] bytes = ReplayUtils.toBytes(array);
-        if (fields[idx].isKey()) {
-            appendToKey(array);
-        }
-        // For now, short array cannot be timestamp field
-        else {
-            row.addValue(bytes);
-        }
-        writeIfNeeded();
+		byte[] bytes = ReplayUtils.toBytes(array);
+		if (fields[idx].isKey()) {
+			appendToKey(array);
+		}
+		// For now, short array cannot be timestamp field
+		else {
+			row.addValue(bytes);
+		}
+		writeIfNeeded();
 	}
 
 	@Override
 	public void writeCharArray(char[] array) throws IOException {
-        byte[] bytes = ReplayUtils.toBytes(array);
-        if (fields[idx].isKey()) {
-            appendToKey(array);
-        }
-        // For now, char array cannot be timestamp field
-        else {
-            row.addValue(bytes);
-        }
-        writeIfNeeded();
+		byte[] bytes = ReplayUtils.toBytes(array);
+		if (fields[idx].isKey()) {
+			appendToKey(array);
+		}
+		// For now, char array cannot be timestamp field
+		else {
+			row.addValue(bytes);
+		}
+		writeIfNeeded();
 	}
 
 	@Override
 	public void writeStringArray(String[] array) throws IOException {
-        byte[] bytes = ReplayUtils.toBytes(array);
-        if (fields[idx].isKey()) {
-            appendToKey(array);
-        }
-        // TODO: For now, string array cannot be timestamp field (might reconsider this !)
-        else {
-            row.addValue(bytes);
-        }
-        writeIfNeeded();
+		byte[] bytes = ReplayUtils.toBytes(array);
+		if (fields[idx].isKey()) {
+			appendToKey(array);
+		}
+		// TODO: For now, string array cannot be timestamp field (might
+		// reconsider this !)
+		else {
+			row.addValue(bytes);
+		}
+		writeIfNeeded();
 	}
 
 	@Override
 	public void writeByteArray(byte[] array) throws IOException {
-        if (fields[idx].isKey()) {
-            appendToKey(array);
-        }
-        // For now, byte array cannot be timestamp field
-        else {
-            row.addValue(array);
-        }
-        writeIfNeeded();
+		if (fields[idx].isKey()) {
+			appendToKey(array);
+		}
+		// For now, byte array cannot be timestamp field
+		else {
+			row.addValue(array);
+		}
+		writeIfNeeded();
 	}
 
-    @Override
-    public void close() throws IOException {
-        storer.disconnect();
-    }
+	@Override
+	public void close() throws IOException {
+		storer.disconnect();
+	}
 }
