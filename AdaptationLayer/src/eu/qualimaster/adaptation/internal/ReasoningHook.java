f19ae170152e28d4d02370191fa9392fbd5d87e8
eichelberger <eichelberger@sse.uni-hildesheim.de>
2016-10-21 22:37:30 +0200

    moving failure output here, more output on reasoning errors

--- AdaptationLayer/src/eu/qualimaster/adaptation/internal/ReasoningHook.java
+++ AdaptationLayer/src/eu/qualimaster/adaptation/internal/ReasoningHook.java
@@ -21,6 +21,7 @@ import java.util.Set;
 import org.apache.log4j.LogManager;
 
 import eu.qualimaster.easy.extension.QmConstants;
+import net.ssehub.easy.instantiation.core.model.vilTypes.configuration.Configuration;
 import net.ssehub.easy.instantiation.rt.core.model.rtVil.IReasoningHook;
 import net.ssehub.easy.instantiation.rt.core.model.rtVil.IRtValueAccess;
 import net.ssehub.easy.instantiation.rt.core.model.rtVil.IRtVilConcept;
@@ -50,22 +51,21 @@ public class ReasoningHook extends ReasoningHookAdapter {
     
     @Override
     public Status analyze(Script script, IRtVilConcept concept, IRtValueAccess values, Message message) {
-        boolean found = false;
+        List<String> found = new java.util.ArrayList<String>();
         List<Set<IDecisionVariable>> probVars = message.getProblemVariables();
         if (null != probVars) {
             for (Set<IDecisionVariable> set : probVars) {
                 for (IDecisionVariable var : set) {
-                    for (int a = 0; !found && a < var.getAttributesCount(); a++) {
+                    for (int a = 0; a < var.getAttributesCount(); a++) {
                         IDecisionVariable attribute = var.getAttribute(a);
                         AbstractVariable decl = attribute.getDeclaration();
                         if (QmConstants.ANNOTATION_BINDING_TIME.equals(decl.getName())) {
                             Value val = attribute.getValue();
                             if (val instanceof EnumValue) {
                                 EnumValue eVal = (EnumValue) val;
-                                found = (eVal.getValue().getName().equals(
-                                    QmConstants.CONST_BINDING_TIME_RUNTIME_ENACT));
-                                if (found) {
-                                    break;
+                                if (eVal.getValue().getName().equals(
+                                    QmConstants.CONST_BINDING_TIME_RUNTIME_ENACT)) {
+                                    found.add(var.getDeclaration().getQualifiedName());
                                 }
                             }
                         }
@@ -74,13 +74,25 @@ public class ReasoningHook extends ReasoningHookAdapter {
             }
         }
         Status result;
-        if (found) {
+        if (!found.isEmpty()) {
             result = message.getStatus(); // pass only if relevant variable found
+            LogManager.getLogger(ReasoningHook.class).error("Failing reasoning message " + toText(message) 
+                + " due to " + found);            
         } else {
             LogManager.getLogger(ReasoningHook.class).warn("Skipped reasoning message: " + toText(message));
             result = null; // ignore
         }
         return result;
+    }
+
+    /**
+     * Is called if reasoning is considered to fail, e.g., to inform the user.
+     * 
+     * @param config the configuration
+     */
+    @Override
+    public void reasoningFailed(Configuration config) {
+        net.ssehub.easy.varModel.confModel.Configuration.printConfig(System.out, config.getConfiguration());
     }
 
 }
