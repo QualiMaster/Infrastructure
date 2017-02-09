765fcc95e5cb8b15dfd85f465d21b8c1c9c0a00d
Tuan Tran <tranatuan24@gmail.com>
2017-02-09 17:26:40 +0100

    fix error in handling missing timestamps

--- DataManagementLayer/src/main/java/eu/qualimaster/dataManagement/sinks/replay/ReplayDataOutput.java
+++ DataManagementLayer/src/main/java/eu/qualimaster/dataManagement/sinks/replay/ReplayDataOutput.java
@@ -293,8 +293,8 @@ public class ReplayDataOutput implements IDataOutput, Closeable {
 				try {
 					timestamp = ReplayUtils.getTimestamp(fields[idx], value);
 				} catch (Exception e) {
-					log.warn("Error in converting timestamp: " + value + ". Return 0L. This might cause loss of replay data");
-					timestamp = 0L;
+					log.warn("Error in converting timestamp: " + value + ". Ignore");
+					hasNull = true;
 				}
 			} else {
 				row.addValue(bytes);
