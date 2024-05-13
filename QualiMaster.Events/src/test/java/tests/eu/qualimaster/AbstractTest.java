ebc9ed598440d9e6d73ecb4135d9509247d4469d
Holger Eichelberger <eichelberger@sse.uni-hildesheim.de>
2024-05-13 15:02:40 +0200

    determining testdata directory

--- QualiMaster.Events/src/test/java/tests/eu/qualimaster/AbstractTest.java
+++ QualiMaster.Events/src/test/java/tests/eu/qualimaster/AbstractTest.java
@@ -16,6 +16,8 @@
 package tests.eu.qualimaster;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 /**
  * Abstract base test.
@@ -24,6 +26,26 @@ import java.io.File;
  */
 public class AbstractTest {
     
-    public static final File TESTDATA = new File(System.getProperty("qm.base.dir", "."), "testdata");
+    public static final File TESTDATA = determineTestDataDir();
 
+    /**
+     * Determines the {@code testdata} directory from {@code qm.base.dir} or from the file {@code testdata.dir}.
+     * 
+     * @return the testdata directory
+     */
+    private static File determineTestDataDir() {
+        File result = new File(System.getProperty("qm.base.dir", "."), "testdata");
+        if (!result.exists()) {
+            File f = new File("testdata.dir");
+            if (f.exists()) {
+                try {
+                    result = new File(new String(Files.readAllBytes(f.toPath())).trim());
+                } catch (IOException e) {
+                    System.out.println("Failed reading testdata.dir: " + e.getMessage());
+                }
+            }
+        }
+        return result;
+    }
+    
 }
